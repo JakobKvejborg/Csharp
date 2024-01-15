4e8faa0321a86d7623ebf6e6c979d0478765fead
JakobKvejborg <you@example.com>
2024-01-15 09:55:41 +0100

    C# .NET project

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/* Create new C# project from console:
+cd PathWhereYou/Want/Your/Project
+mkdir ProjectName (creates new folder)
+cd ProjectName (changes the folder to the project folder) 
+dotnet new console (does some magic shit with .NET and C#)
+
+code . (opens the new project in vs code)
+
+Run the program from Terminal:
+dotnet run
+*/
+
+Console.WriteLine("Hello, World!");
